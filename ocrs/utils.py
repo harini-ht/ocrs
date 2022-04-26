9bd0830753cac9f02f49e559a2bddf1e36d47fbb
harini-ht <harini.jt@gmail.com>
2022-04-26 23:16:41 +0530

    init ✅

--- ocrs/utils.py
+++
@@ -1,3 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://hash:Harini123!@#@cluster0.lqkra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = client['ocrs']
---
+++ ocrsApp/admin.py
@@ -0,0 +1,25 @@
+from .models import Profile
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+
+
+# Only display the "username" field
+fields = ["username"]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
